7d8bc457710409f51b01259b720e8c33aa122851
Tomasz Jagas <tomasz.jagas@gmail.com>
2021-02-13 22:42:03 +0100

    first commit

---
+++ c-programming-088-stringIOChallange.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF 256
+
+int hasCh(char ch, const char *line);
+
+int main(int argc, char *argv[]) {
+    FILE *fp = NULL;
+    char ch = '\0';
+    char line[BUF];
+
+    if (argc != 3) {
+        printf("Usage: %s character filename\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    ch = argv[1][0];
+
+    if ((fp = fopen(argv[2], "r")) == NULL) {
+        printf("Can't open %s\n", argv[2]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, BUF, fp) != NULL) {
+        if (hasCh(ch, line)) {
+            fputs(line, stdout);
+        }
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+int hasCh(char ch, const char *line) {
+    while (*line) {
+        if (ch == *line++){
+            return 1;
+        }
+    }
+    return 0;
+}
